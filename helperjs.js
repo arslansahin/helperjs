04880212447d230d913e022dc8e5bddf8add74df
arslansahin <arsann@gmail.com>
2017-07-13 09:37:03 +0300

    date metodu ve özelliği güncellendi

--- helperjs.js
+++ helperjs.js
@@ -336,11 +336,10 @@ window.addEventListener("load",function(event) {
     //date
     if(x.getAttribute('date') == 'true'){
       try {
-
         var today = helperjs.date({
-          seperator : x.getAttribute('date-seperator')
+          seperator : x.getAttribute('date-seperator') ? x.getAttribute('date-seperator') : '',
+          format : x.getAttribute('date-format') ? x.getAttribute('date-format') : ''
         });
-
         if(elementArray.indexOf(x.nodeName.toLowerCase()) > -1){
             x.innerHTML = today;
         }
@@ -350,17 +349,13 @@ window.addEventListener("load",function(event) {
         else if(x.nodeName.toLowerCase() == 'textarea' ){
           x.textContent = today;
         }
-
       } catch (e) {
-        console.log(e);
-      } finally {
-
+        //console.log(e);
       }
     }
   });
   document.querySelector('html').style.display='block';
 },false);
-
 var helperjs = {
   ajax : function(obj){
     /*
@@ -486,6 +481,7 @@ var helperjs = {
     minutes = minutes < 10 ? "0" + minutes : minutes;
     seconds = seconds < 10 ? "0" + seconds : seconds;
     data.display.textContent = minutes + ":" + seconds;
+    var i = 1;
     setInterval(function () {
       minutes = parseInt(timer / 60, 10)
       seconds = parseInt(timer % 60, 10);
@@ -494,7 +490,8 @@ var helperjs = {
       data.display.textContent = minutes + ":" + seconds;
       if (--timer < 0) {
         timer = 0;
-        data.callback && data.callback();
+        i == 1 && data.callback && data.callback();
+        i = 0;
       }
     }, 1000);
   },
@@ -504,6 +501,9 @@ var helperjs = {
     (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
     return check;
   },
+  //date formats
+  //default : DDMMYYYY
+  //YYYYMMDD || YYYYDDMM || MMDDYYYY
   date : function(data){
     var seperator = data.seperator ? data.seperator : '.';
     var date = new Date();
@@ -512,7 +512,18 @@ var helperjs = {
     var month = date.getMonth()+1;
     month = (month < 10 ? '0':'')+month;
     var year = date.getFullYear();
-    var today = day+seperator+month+seperator+year;
+    if(data.format.toLowerCase() == 'yyyymmdd'){
+      var today = year+seperator+month+seperator+day;
+    }
+    else if(data.format.toLowerCase() == 'yyyyddmm'){
+      var today = year+seperator+day+seperator+month;
+    }
+    else if(data.format.toLowerCase() == 'mmddyyyyy'){
+      var today = month+seperator+day+seperator+year;
+    }
+    else {
+      var today = day+seperator+month+seperator+year;
+    }
     return today;
   },
   //md5 encryption
